abb3020d7e2c4268b0fe77d10f885af8032378b1
Joseph-Thur <69287038+Zenokue@users.noreply.github.com>
2021-07-27 19:56:19 +1000

    Question 2 Answers

--- src/Test2.java
+++ src/Test2.java
@@ -1,26 +1,38 @@
+import java.util.*;
+
 public class Test2 {
 
-
-    public static int perm(int val) {
-        int result = 1;
-        while(val > 0) {
-
-            result *= val;
-            val--;
-        }
-
-        return result;
+    private static void swap(char[] e, int a, int b) {
+        char tmp = e[a];
+        e[a] = e[b];
+        e[b] = tmp;
     }
 
+    private static void printArray(char[] e) {
+        for(int i = 0; i < e.length; i++) {
+            System.out.print(e[i]);
+        }
+        System.out.print("\n");
+    }
 
-    public static void calculate() {
-        char[] arr = {'c', 'a', 't', 'd', 'o', 'g'};
-
-        
-        
+    public static void calculate(int n, char[] e) {
+        if(n == 1) {
+            printArray(e);
+        } else {
+            for(int i = 0; i < n-1; i++) {
+                calculate(n - 1, e);
+                if(n % 2 == 0) {
+                    swap(e, i, n-1);
+                } else {
+                    swap(e, 0, n-1);
+                }
+            }
+            calculate(n - 1, e);
+        }
     }
 
     public static void main(String[ ] args) {
-        System.out.println(perm(4));
+        char[] arr = {'c', 'a', 't', 'd', 'o', 'g'};
+        calculate(6, arr);
     }
 }
